33e087aa170e2b57417175c061431979db4e34fa
BalasriMukkala <162893974+BalasriMukkala@users.noreply.github.com>
2024-09-05 15:27:00 +0530

    Update App.jsx
    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Balasri Durga Satyavathi Mukkala",
+  title: "Frontend Developer",
+  email: "balasrimukkala82@gmail.com",
+  gitHub: "https://github.com/BalasriMukkala",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/balasri-mukkala-a79937315/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
